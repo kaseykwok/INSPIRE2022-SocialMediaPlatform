0ed5472c1d7471529279a9991f411564efaf2804
kaseykwok <kaseykwok@gmail.com>
2022-07-02 12:52:13 +0800

    Login page UI

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    </div> -->
+    <b-navbar type="dark" variant="dark" id="nav">
+      <b-navbar-brand>GirlsOnly</b-navbar-brand>
+    </b-navbar>
+    <router-view id="router-app"/>
   </div>
 </template>
 
@@ -13,20 +16,16 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
+  padding: 10px 20px 10px 20px;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+#router-app {
+  padding: 100px;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="login-page">
+    <h3>Welcome to GirlsOnly! Please sign in or register for an account.</h3>
+    <b-form @submit.prevent="loginSubmit">
+      <b-input-group class="my-5" style="height:30px">
+        <b-input-group-prepend is-text>
+          <b-icon icon="person-fill" style="height:30px"></b-icon>
+        </b-input-group-prepend>
+        <b-form-input
+          type="text"
+          required
+          placeholder="Username"
+          v-model="username"
+        ></b-form-input>
+      </b-input-group>
+      <b-input-group class="my-5" style="height:30px">
+        <b-input-group-prepend is-text>
+          <b-icon icon="lock-fill" style="height:30px"></b-icon>
+        </b-input-group-prepend>
+        <b-form-input
+          type="password"
+          required
+          placeholder="Password"
+          v-model="password"
+        >
+        </b-form-input>
+      </b-input-group>
+      <b-button type="submit" variant="primary">Sign in</b-button>
+      <b-button variant="secondary" class="mx-3">Register</b-button>
+    </b-form>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Login',
+  components: {
+    // HelloWorld
+  },
+  data() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    loginSubmit(e) {
+      console.log(this.username)
+      console.log(this.password)
+    },
+  }
+}
+</script>
